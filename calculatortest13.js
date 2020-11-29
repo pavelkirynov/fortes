2b02faf94c9b51079134ae8153c5bb78080d8175
alverone <48132231+alverone@users.noreply.github.com>
2020-11-29 13:56:39 +0200

    Update calculatortest13.js

--- calculatortest13.js
+++ calculatortest13.js
@@ -80,6 +80,7 @@ const onDataLoaded = (data) => {
                 }
                 if (sign === ">=") {
                     if (+comparableData >= +threshold) {
+                        console.log("threshold " + threshold + " sign " + sign + " value " + value + " price " + price + " data " + comparableData);
                         price = value;
                     }
                 }
@@ -616,7 +617,7 @@ const onDataLoaded = (data) => {
 
         let months = parseFloat(parseData("D8", space));
         console.log(handleAppliances(appliances) + " " + handleConditioning(conditioning, ClimaticMarkup) + " " + handleFurniture(furniture, FurnitureMarkup) + " " + (handleMaterials(materials) + " " + handleWork(work, months) + " " + AccessorriesMarkup + " " + handleOptions(options) + " " + space));
-        return (parseInt(handleAppliances(appliances) * 0.9) + parseInt(handleConditioning(conditioning, ClimaticMarkup)) + parseInt(handleFurniture(furniture, FurnitureMarkup)) + parseInt(((handleMaterials(materials) + handleWork(work, months)) * (1 + (AccessorriesMarkup / 100)))) + parseInt(handleOptions(options))) / (28.5 * space);
+        return (parseInt(handleAppliances(appliances)) * 0.9 + parseInt(handleConditioning(conditioning, ClimaticMarkup)) + parseInt(handleFurniture(furniture, FurnitureMarkup)) + parseInt(((handleMaterials(materials) + handleWork(work, months)) * (1 + (AccessorriesMarkup / 100)))) + parseInt(handleOptions(options))) / (28.5 * space);
     }
 
     $("input")
@@ -689,5 +690,5 @@ const onDataLoaded = (data) => {
     });
 
 
-    $("#total").html(0);
+    $("#total").html(handleTotal());
 };
