bac55c1a1684c1f84906aa9c9c70607454fc6edd
alverone <48132231+alverone@users.noreply.github.com>
2020-12-06 17:30:38 +0200

    Update and rename info15.js to info16.js

--- info16.js
+++ info16.js
@@ -14,26 +14,18 @@ const onDataLoaded = (data) => {
         threshold = 0;
 
     function parseData(range, comparableData) {
-        //console.log(comparableData);
         let rawData = data.feed.entry.find((entry) => entry.title.$t == range).gs$cell.inputValue;
         if (rawData.includes("=IF")) {
             while (rawData[rawData.length - 1] == ")") {
                 rawData = rawData.substring(0, rawData.length - 1);
-                //console.log("e");
             }
             let formulaData = rawData.split("IF");
-            //console.log(Fdata);
             formulaData = formulaData.map(item => item.split(";"));
-            //console.log(formulaData[1][1]);
             for (let i = 1; i < formulaData.length; i++) {
-                //console.log(formulaData.length);
-                //console.log(i);
                 value = parseFloat(formulaData[i][1]);
-                //console.log(value);
 
                 let item = formulaData[i][0].slice(1, formulaData[i][0].length).split("=");
                 threshold = formulaData[i][0].slice(1, formulaData[i][0].length).split("=")[1];
-                //console.log(threshold);
                 if (item[0].indexOf(">") > -1) {
                     sign = morethan;
                 } else if (item[0].indexOf("<") > -1) {
@@ -41,31 +33,21 @@ const onDataLoaded = (data) => {
                 } else {
                     sign = equals;
                 }
-                //console.log(sign);
                 if (!price) {
                     price = value;
                 }
                 if (sign === "<=") {
                     if (+comparableData <= +threshold) {
                         price = value;
-                        console.log("threshold " + threshold + " sign " + sign + " value " + value + " price " + price + " data " + comparableData);
                         return price;
                     }
                 }
                 if (sign === ">=") {
                     if (+comparableData >= +threshold) {
-                        console.log("threshold " + threshold + " sign " + sign + " value " + value + " price " + price + " data " + comparableData);
                         price = value;
                     }
                 }
-                /*console.log(formulaRaw);
-                console.log(formulaRaw[0][2] + " test");
-                value = formulaRaw[0][3];
-                sign = formulaRaw[0][2];*/
-                //formulaRaw.splice(0, formulaRaw.length);
-                //return price;
             }
-            //console.log(price);
             return price;
         }
         return data.feed.entry.find((entry) => entry.title.$t == range).content.$t;
@@ -109,6 +91,13 @@ const onDataLoaded = (data) => {
                 delivery: parseData("C201"),
             },
         };
+    for (let i = 0; i < 6; i++) {
+        let $container = $(".list-container");
+        let $con
+        $container.append("<div class=\"division-block\"></div>");
+        $container.append("<div class=\"option-block\"></div>");
+        $container.last(".list-option.container").append(`<span class=\"name\">${parseData("F"+(72+i))}, ${parseData("G"+(72+i))}</span><span class=\"list-text\">${parseData("H"+(72+i))} грн.</span>`);
+    }
     let cookies = document.cookie.split(";").map((cookie) => cookie.split("=")).reduce((accumulator, [key, value]) => ({...accumulator,[key.trim()]: decodeURIComponent(value),}),{});
     let appliancesCookie = cookies._appliances;
     if (!!appliancesBoolTotal) {
@@ -140,12 +129,5 @@ const onDataLoaded = (data) => {
             }
             i = 1;
         }
-    }
-        for (let i = 0; i < 6; i++) {
-        let $container = $(".list-container").first();
-        let $con
-        $container.append("<div class=\"division-block\"></div>");
-        $container.append("<div class=\"option-block\"></div>");
-        $container.last(".list-option.container").append(`<span class=\"name\">${parseData("F"+(72+i))}, ${parseData("G"+(72+i))}</span><span class=\"list-text\">${parseData("H"+(72+i))} грн.</span>`);
     }
 };
