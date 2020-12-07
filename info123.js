2fda7ac8a3ab04485c3bcecb76ae1cd315bd2dce
alverone <48132231+alverone@users.noreply.github.com>
2020-12-07 14:15:28 +0200

    Update and rename info122.js to info123.js

--- info123.js
+++ info123.js
@@ -169,9 +169,9 @@ const onDataLoaded = (data) => {
         appendWorkOption(parseData("F48"), parseData("G48"), 1, parseFloat(parseData(`${letter + 48}`, space)) * space);
         appendWorkOption(parseData("F49"), parseData("G49"), amountOfBathrooms, parseFloat(parseData(`${letter + 49}`, space)) * space);
         appendWorkOption(parseData("F50"), parseData("G50"), (parseFloat(amountOfBathrooms) + parseFloat(amountOfRooms)), parseFloat(parseData(`${letter + 50}`)) * (parseFloat(amountOfRooms) + parseFloat(amountOfBathrooms)));
-        appendWorkOption(parseData("F52"), parseData("G52"), 1, parseFloat(parseData(`${letter + 52}`, space)) * space);
-        appendWorkOption(parseData("F54"), parseData("G54"), 1, parseFloat(parseData(`${letter + 54}`)));
-        appendWorkOption(parseData("F53"), parseData("G53"), mouldings, parseFloat(parseData(`${letter + 53}`, space)) * 140);        
+        appendWorkOption(parseData("G51"), parseData("G52"), 1, parseFloat(parseData(`${letter + 52}`, space)) * space);
+        appendWorkOption(parseData("G51"), parseData("G54"), 1, parseFloat(parseData(`${letter + 54}`)));
+        appendWorkOption(parseData("G51"), parseData("G53"), mouldings, parseFloat(parseData(`${letter + 53}`, space)) * 140);        
         appendWorkOption(parseData("F"+ceilingNum), parseData("G" + ceilingNum), 1, parseFloat(parseData(letter + ceilingNum, space)) * space);
         appendWorkOption(parseData("F" + flooringNum), parseData("G" + flooringNum), 1, parseFloat(parseData(letter + flooringNum, space)) * space);
         appendWorkOption(parseData("F64"), parseData("G64"), 1, parseFloat(parseData(`${letter + 64}`, space)) * space);
@@ -186,14 +186,14 @@ const onDataLoaded = (data) => {
             workSum += parseFloat(price);
             $work.append("<div class=\"option-block\"><div class=\"division-block pricelist\"></div><div class=\"list-option-container\"></div></div>");
             if (!manufacturer) {
-               $("#workList .option-block .list-option-container").last().append(`<span class=\'name\'>${name}</span><span class=\'list-text amount-work\'>${amount} шт.</span>`);
+               $("#workList .option-block .list-option-container").last().append(`<span class=\'name\'>${name}</span>`);
                     return;
             }            
-            $("#workList .option-block .list-option-container").last().append(`<span class=\'name\'>${name}, ${manufacturer}</span><span class=\'list-text amount-work\'>${amount} шт.</span>`);
+            $("#workList .option-block .list-option-container").last().append(`<span class=\'name\'>${name}, ${manufacturer}</span>`);
     }
         //$("#workList").append("<div class=\"division-block pricelist\"></div><div class=\"list-option-container summary\"></div>");
         //$("#workList .list-option-container").last().append(`<span class=\'name summary\'>Всього по будівельній частині:</span><span class=\'list-text summary work\'>${workSum} грн.</span>`);
-    /*<span class=\'list-text work\'>${price} грн.</span>*/
+    /*<span class=\'list-text work\'>${price} грн.</span><span class=\'list-text amount-work\'>${amount} шт.</span>*/
     if (!appliancesBoolTotal) {
         $(".comfy-section").css("display", "none");
         $("#appliancesListTotal").css("display", "none");
