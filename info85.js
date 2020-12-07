b0da1e6d73eaae9fee2ef561017a618ae6017d6c
alverone <48132231+alverone@users.noreply.github.com>
2020-12-07 09:15:48 +0200

    Update and rename info84.js to info85.js

--- info85.js
+++ info85.js
@@ -149,7 +149,7 @@ const onDataLoaded = (data) => {
                $("#workList .option-block .list-option-container").last().append(`<span class=\'name\'>${name}</span><span class=\'list-text amount\'>${amount}</span><span class=\'list-text work\'>${price} грн.</span>`);
                     return;
             }*/
-            $("#workList .option-block .list-option-container").last().append(`<span class=\'name\'>${name}, ${manufacturer}</span><span class=\'list-text amount\'>${amount}</span><span class=\'list-text work\'>${price} грн.</span>`);
+            $("#workList .option-block .list-option-container").last().append(`<span class=\'name\'>${name}</span><span class=\'list-text amount\'>${amount}</span><span class=\'list-text work\'>${price} грн.</span>`);
     }
     
     if (!appliancesBoolTotal) {
