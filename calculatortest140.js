a52999c83dd23d6ee7e041e4a8e8779db6c1826e
alverone <48132231+alverone@users.noreply.github.com>
2020-12-22 22:04:45 +0200

    Update and rename calculatortest139.js to calculatortest140.js

--- calculatortest140.js
+++ calculatortest140.js
@@ -141,6 +141,7 @@
             paintingTotal = element.wallPainting * space + element.ceilingPainting * ceilingBool.ceiling3 * space + element.mouldings;
             plinthTotal = (element.plinth + element.plinthHidden) * space;
             let workTotal = (generalTotal + ceilingTotal + flooringTotal + paintingTotal + plinthTotal) + (generalTotal + ceilingTotal + flooringTotal + paintingTotal + plinthTotal) * 0.022 + ((space * 100) + (parseData("G8", space) * 2 * 1200) + 3000);
+            console.log(generalTotal +" " + ceilingTotal +" " + flooringTotal +" " + paintingTotal +" " + plinthTotal +" " + (generalTotal + ceilingTotal + flooringTotal + paintingTotal + plinthTotal) * 0.022 +" " + ((space * 100) + (parseData("G8", space) * 2 * 1200) + 3000));
             console.log("WORK " + workTotal/(28.5*50));
             return workTotal;
         }
