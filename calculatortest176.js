d79d30c53388ea272069ef85bdbb221794c012d3
alverone <48132231+alverone@users.noreply.github.com>
2020-12-23 17:16:20 +0200

    Update and rename calculatortest175.js to calculatortest176.js

--- calculatortest176.js
+++ calculatortest176.js
@@ -841,7 +841,7 @@
                 style = "neoclassic";
             }
             $(".calculator-tab").removeClass("w--current");
-            $(`.calculator-tab[data-slider-index='${slideNumber}']`).addClass("w--current");
+            $('.calculator-tab[data-slider-index='0']').addClass("w--current");
             $("#total").html(handleTotal().toFixed(2));
             $("#totalWhole").html(Math.round(handleTotal() * space * 28.5));
             $(".calculator-slider-side").slick("slickGoTo", 0);
@@ -855,9 +855,9 @@
             $(".calculator-tab[data-slider-index='0']").addClass("w--current");
             $("#total").html(handleTotal().toFixed(2));
             $("#totalWhole").html(Math.round(handleTotal() * space * 28.5));
+            $(".calculator-slider-side").slick("slickGoTo", 0);
             $(".calculator-slide").toggle(false);
             $(".calculator-slide.main, .calculator-slide.cozy").toggle(true);
-            $(".calculator-slider-side").slick("slickGoTo", 0);
             $(".calculator-slider-option.active").removeClass("active");
             $(".calculator-slider-option[data-slider-index='0']").addClass("active");
         });
