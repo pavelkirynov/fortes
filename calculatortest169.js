5e3c237cee6832df7aeb9786184e48f077f810d8
alverone <48132231+alverone@users.noreply.github.com>
2020-12-23 16:17:35 +0200

    Update and rename calculatortest168.js to calculatortest169.js

--- calculatortest169.js
+++ calculatortest169.js
@@ -844,9 +844,9 @@
             $(`.calculator-tab[data-slider-index='${slideNumber}']`).addClass("w--current");
             $("#total").html(handleTotal().toFixed(2));
             $("#totalWhole").html(Math.round(handleTotal() * space * 28.5));
-            styleSlide = $(`.calculator-slide.${style}`);
+            let styleSlide = $(`.calculator-slide.${style}`);
             $(".calculator-slide").toggle(false);
-            $(`.calculator-slide.main`).toggle(true);
+            $(".calculator-slide.main").toggle(true);
             styleSlide.toggle(true);
         });
         $(".choice").on("click", function () {
