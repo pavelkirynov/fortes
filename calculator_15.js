a53a52d89c5892da5b2719d2d98fd5207dd66cda
alverone <48132231+alverone@users.noreply.github.com>
2021-01-03 19:53:55 +0200

    Update and rename calculator_14.js to calculator_15.js

--- calculator_15.js
+++ calculator_15.js
@@ -100,7 +100,7 @@
             decorationsTotal = element.jalousie + element.coffeeTable * (amountOfRooms - 1) + (element.cornice + element.tulle + element.curtains) * amountOfRooms;
             let showPrice = (element.coffeeTable * (amountOfRooms - 1) + (element.cornice + element.tulle + element.curtains) * amountOfRooms + element.bed + element.matress + element.shelves * 2 + element.kitchenCeilingLight * 2 + element.bedsideLight * 2 + element.chandelier) * (1 + (parseData("S157") / 100));
             let furnitureTotal = (kitchenTotal + livingroomTotal + bedroomTotal + lightingTotal +
-                    decorationsTotal) + (kitchenTotal + livingroomTotal + bedroomTotal + lightingTotal +
+                    decorationsTotal) * (1 + (parseData("S157") / 100)) + (kitchenTotal + livingroomTotal + bedroomTotal + lightingTotal +
                     decorationsTotal) * 0.03 * (1 + (parseData("S157") / 100));
             $("#furnitureBool").siblings(".label").html(`Так <span class=\"grey\">+${returnRoundedPrice(furnitureTotal - showPrice)}$/м²</span>`);
             if (furnitureBool) {
