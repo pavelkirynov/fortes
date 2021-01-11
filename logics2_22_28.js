36920f640124410fcde62fa0ab00a61f7d272fe7
alverone <48132231+alverone@users.noreply.github.com>
2021-01-11 22:24:16 +0200

    Update and rename logics2_22_27.js to logics2_22_28.js

--- logics2_22_28.js
+++ logics2_22_28.js
@@ -141,16 +141,16 @@ function startAnimation() {
         return;    
     }
     $bar.parent().css("opacity", 0);
-    $(`.progressbar:eq(${barIndex})`).parent().css("opacity", 1);
-    $(`.progressbar:eq(${barIndex})`).animate({
-        width: "100%",
-    }, 10000, "swing", function () {
-            stopAnimation();
         if (barIndex >= 4) {
             barIndex = 0;
         } else {
             barIndex++;
         }
+    $(`.progressbar:eq(${barIndex})`).parent().css("opacity", 1);
+    $(`.progressbar:eq(${barIndex})`).animate({
+        width: "100%",
+    }, 10000, "swing", function () {
+            stopAnimation();
         t[0].slick.slickGoTo(barIndex);
         startAnimation();
     });
