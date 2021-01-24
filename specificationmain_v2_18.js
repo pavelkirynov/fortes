ba090d8fae3f987ee194f76382822329f6d1250c
alverone <48132231+alverone@users.noreply.github.com>
2021-01-24 19:10:26 +0200

    Update and rename specificationmain_v2_17.js to specificationmain_v2_18.js

--- specificationmain_v2_18.js
+++ specificationmain_v2_18.js
@@ -145,7 +145,7 @@ const onDataLoaded = (data) => {
         vents = parseFloat(parseData(`${letter+44}`, space)) * space,
         electricity = parseFloat(parseData(`${letter+45}`, space)) * space;
 
-    let workPriceArray = [water, canalisation, electricity, vents, parseFloat(parseData(`${letter + 46}`, space)), parseFloat(parseData(`${letter + 47}`, space)), parseFloat(parseData(`${letter + 48}`, space)) * space, parseFloat(parseData(`${letter + 49}`, space)) * space, parseFloat(parseData(`${letter + 50}`)), parseFloat(parseData(`${letter + 52}`, space)) * space, parseFloat(parseData(`${letter + 54}`)), parseFloat(parseData(`${letter + 53}`, space)) * 140, parseFloat(parseData(letter + ceilingNum, space)) * space, parseFloat(parseData(letter + flooringNum, space)) * space, parseFloat(parseData(`${letter + 64}`, space)) * space, parseFloat(parseData(`${letter + 65}`, space)) * space, Math.round((parseFloat(workSum) / 100) * 1.56), (parseFloat(parseData("G8", space) * 2 * 1200) + 3000 + (space * 100))];
+    let workPriceArray = [water, canalisation, electricity, vents, parseFloat(parseData(`${letter + 46}`, space)), parseFloat(parseData(`${letter + 47}`, space)), parseFloat(parseData(`${letter + 48}`, space)) * space, parseFloat(parseData(`${letter + 49}`, space)) * space, parseFloat(parseData(`${letter + 50}`)), parseFloat(parseData(`${letter + 52}`, space)) * space, parseFloat(parseData(`${letter + 54}`)), parseFloat(parseData(`${letter + 53}`, space)) * 140, parseFloat(parseData(letter + ceilingNum, space)) * space, parseFloat(parseData(letter + flooringNum, space)) * space, parseFloat(parseData(`${letter + 64}`, space)) * space, parseFloat(parseData(`${letter + 65}`, space)) * space, (workSum - vents - canalisation - water - electricity) * 0.022, (parseFloat(parseData("G8", space) * 2 * 1200) + 3000 + (space * 100))];
     let workAmountArray = [1, 1, parseFloat(amountOfBathrooms), 1, parseInt(shower), parseInt(bath), 1, amountOfBathrooms, (parseFloat(amountOfRooms) + parseFloat(amountOfBathrooms)), 1, 1, mouldings, 1, 1, 1, 1, 1, 1];
     let workAdressesArray = [42, 43, 44, 45, 46, 47, 48, 49, 50, 52, 54, 53, ceilingNum, flooringNum, 64, 65, 66, 67];
 
@@ -444,5 +444,4 @@ textObject = `<div class=\"option-block\"><div class=\"division-block pricelist\
     $("#kitchenTotalPriceDiscount").html(spacedNum(Math.round(sum * 0.9)));
     $("#discountTotal").html(`<span class='bold-text-7'>${spacedNum(Math.round(sum - sum * 0.9))} грн.</span>`);
     $("#totalPriceTotal").html(spacedNum(Math.round(furnitureSum + workSum + parseInt(appliancesBoolTotal) * sum * 0.9)) + " грн. *");
-    console.log("test" + furnitureSum + " " + workSum + " " + furnitureSum + " " + Math.round(sum * 0.9));
 };
