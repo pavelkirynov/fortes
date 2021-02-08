7858828965dcd3e10c820a505a0058e9f35233d0
alverone <48132231+alverone@users.noreply.github.com>
2021-02-08 13:25:01 +0200

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -73,11 +73,8 @@
     });
     $(".calculator-tab").on("click", function () {
         number = parseInt($(this).data("slider-index"));
-        $(".calculator-slide").toggle(false);
-        $(".calculator-slide.main").toggle(true);
-        $(`.calculator-slide.` + data.style).toggle(true);
         getUserStyle(number);
-        console.log(data.style);
+
         returnValue(data.space);
     });
     $("#calculate").on("click", function () {
