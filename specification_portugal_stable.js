3629e778c1e5f9a2c085344cd1e89e14855ddbb0
eszeit <artem@freshcut.gg>
2022-10-06 12:30:53 +0300

    portugal changes

--- specification_portugal_stable.js
+++ specification_portugal_stable.js
@@ -1,5 +1,5 @@
 fetch(
-  "https://docs.google.com/spreadsheets/d/1KkkpKbytztt48mwP1RGgpVFpfke8-IqB0KLWA8Sn2FE/gviz/tq?tqx=out:json"
+  "https://docs.google.com/spreadsheets/d/1KkkpKbytztt48mwP1RGgpVFpfke8-IqB0KLWA8Sn2FE/gviz/tq?tqx=out:json&gid=602865824"
 )
   .then((res) => res.text())
   .then((text) => {
